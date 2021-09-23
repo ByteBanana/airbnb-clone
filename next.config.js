878c17a262b6ff27b6dc42879d532e927ecbccca
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2021-09-23 23:16:02 +0700

    Remove pubk

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
   },
-  env: {
-    mapbox_key:
-      'pk.eyJ1Ijoic2FoYXJhdHBheW5vayIsImEiOiJjanNyMDd6cjgxZDdoNDlwZ3E3ZGt3aHZnIn0.f75QOVGwSt1_q9OIO6W93w',
-  },
 };
